d2901b8f591858895f3d7f69a4dc89488a310981
Thiago <thiagofang@gmail.com>
2023-02-02 20:06:33 -0300

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import ReactFlow, { Background, Controls } from "reactflow";
+import ReactFlow, { Background, Controls, Node } from "reactflow";
 import { zinc } from "tailwindcss/colors";
+
+import { Square } from "./Components/Nodes/Square/Square";
+
 import "reactflow/dist/style.css";
+
+const NODE_TYPES = {
+  square: Square,
+};
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+] satisfies Node[];
 
 function App() {
   return (
     <div className="w-screen h-screen">
-      <ReactFlow>
+      <ReactFlow nodeTypes={NODE_TYPES}>
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
