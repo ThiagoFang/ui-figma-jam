c9b4c02276c7cbf296600cc9a60659b6e1e4c162
Thiago <thiagofang@gmail.com>
2023-02-02 19:45:39 -0300

    tailwind configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import ReactFlow, { Background, Controls } from "reactflow";
+import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
 
 function App() {
   return (
-    <div className="maindiv">
+    <div className="w-screen h-screen">
       <ReactFlow>
-        <Background gap={12} size={2} color="#DDD" />
+        <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
       </ReactFlow>
     </div>
   );
