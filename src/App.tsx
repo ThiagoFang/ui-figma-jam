9ab6cda35fa963b95b9c05cc3dc79846b2c9bbda
Thiago <thiagofang@gmail.com>
2023-02-02 21:05:36 -0300

    creating floating menu and function to create new nodes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import ReactFlow, {
   useNodesState,
 } from "reactflow";
 import { zinc } from "tailwindcss/colors";
+import * as ToolBar from "@radix-ui/react-toolbar";
 
 import { Square } from "./Components/Nodes/Square/Square";
 
@@ -53,6 +54,21 @@ function App() {
     return setEdges((edges) => addEdge(connection, edges));
   }, []);
 
+  const addSquareNode = () => {
+    setNodes((nodes) => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: "square",
+        position: {
+          x: 750,
+          y: 350,
+        },
+        data: {},
+      },
+    ]);
+  };
+
   return (
     <div className="w-screen h-screen">
       <ReactFlow
@@ -71,6 +87,13 @@ function App() {
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
+
+      <ToolBar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
+        <ToolBar.Button
+          onClick={addSquareNode}
+          className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2"
+        />
+      </ToolBar.Root>
     </div>
   );
 }
